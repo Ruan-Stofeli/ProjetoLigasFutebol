d6bd56a4be7544eeb68014c405c82f9f8361d5ce
ruanbritania <stofeli.ruan@gmail.com>
2026-04-27 23:28:24 -0300

    Upload de arquivos

---
+++ QueryInserirCompeticoes.sql
@@ -0,0 +1,8 @@
+INSERT INTO Competicoes (CompeticaoID,Nome,Pais,TipoCompeticao,AnoFundacao,Organizador,Nivel,LogoURL,Formato)
+VALUES
+(1, 'La Liga','ESP','Liga',1929,'RFEF',1,'https://upload.wikimedia.org/wikipedia/commons/0/0f/LaLiga_logo_2023.svg','Pontos Corridos'),
+(2, 'Premier League','ENG','Liga',1992,'The Football Association',1,'https://upload.wikimedia.org/wikipedia/commons/b/bf/Pl-logo-light.svg','Pontos Corridos'),
+(3, 'Serie A','ITA','Liga',1898,'FIGC',1,'https://upload.wikimedia.org/wikipedia/commons/e/e9/Serie_A_logo_2022.svg','Pontos Corridos'),
+(4, 'Bundesliga','GER','Liga',1963,'DFB',1,'https://upload.wikimedia.org/wikipedia/commons/3/31/Bundesliga_Wordmark_%282010-2017%29.svg','Pontos Corridos'),
+(5, 'Ligue 1','FRA','Liga',1932,'LFP',1,'https://upload.wikimedia.org/wikipedia/commons/a/a0/Ligue_1_2024_Logo.png','Pontos Corridos');
+	
